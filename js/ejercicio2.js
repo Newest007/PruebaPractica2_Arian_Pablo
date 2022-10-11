0a3b0875fa5f9c301d502541e10e3fdb2af131f5
Pablo Velasco <pablo.velasco@gmail.com>
2022-10-10 23:51:52 -0600

    Tabla creada, falta comprobación

--- js/ejercicio2.js
+++ js/ejercicio2.js
@@ -1,13 +1,11 @@
 document.addEventListener("DOMContentLoaded", function(){
 
-// Example starter JavaScript for disabling form submissions if there are invalid fields
+//Validaciones de campos vacios de bootstrap
 (() => {
     'use strict'
   
-    // Fetch all the forms we want to apply custom Bootstrap validation styles to
     const forms = document.querySelectorAll('.needs-validation')
   
-    // Loop over them and prevent submission
     Array.from(forms).forEach(form => {
       form.addEventListener('submit', event => {
         if (!form.checkValidity()) {
@@ -18,7 +16,7 @@ document.addEventListener("DOMContentLoaded", function(){
         form.classList.add('was-validated')
       }, false)
     })
-  })()
+})()
 
 //Definimos los arreglos y la variable que utilizazremos
 let nombreEquipo;
@@ -33,208 +31,86 @@ const btnEquipo = document.querySelector("#btnEquipo");
 const btnPortero = document.querySelector("#btnGuardarPortero");
 const btnDefensa = document.querySelector("#btnGuardarDefensas");
 const btnMedios = document.querySelector("#btnGuardarMedios");
-const btndelante = document.querySelector("#btnGuardarDelante");
+const btnDelante = document.querySelector("#btnGuardarDelante");
 
-/*A continuación añadiremos acciones a los input para que solo se pueda hacer click en el botón 
-correspondiente si solo si, se han llenado cada uno de los campos en ese apartado */
+//Agregamos los eventos a los respectivos botones
+btnEquipo.addEventListener("click", addEquipo);
+btnPortero.addEventListener("click", addPortero);
+btnDefensa.addEventListener("click", addDefensas);
+btnMedios.addEventListener("click", addMedios);
+btnDelante.addEventListener("click", addDelante);
 
-//Para el portero
-document.getElementById("inputNombrePortero").addEventListener("keyup", validarPortero);
-document.getElementById("inputCarnetPortero").addEventListener("keyup", validarPortero);
-document.getElementById("inputEdadPortero").addEventListener("keyup", validarPortero);
-document.getElementById("selectSexoPortero").addEventListener("change", validarPortero);
+//Codificamos las funciones antes colocadas
 
-//Para los defensas
-document.getElementById("inputNombrePortero").addEventListener("keyup", validarPortero);
-document.getElementById("inputCarnetPortero").addEventListener("keyup", validarPortero);
-document.getElementById("inputEdadPortero").addEventListener("keyup", validarPortero);
-document.getElementById("selectSexoPortero").addEventListener("change", validarPortero);
+function addEquipo(){
+    nombreEquipo = document.querySelector("#inputNombreEquipo").value.toString();
+}
 
-//Para los mediocampistas
-document.getElementById("inputNombrePortero").addEventListener("keyup", validarPortero);
-document.getElementById("inputCarnetPortero").addEventListener("keyup", validarPortero);
-document.getElementById("inputEdadPortero").addEventListener("keyup", validarPortero);
-document.getElementById("selectSexoPortero").addEventListener("change", validarPortero);
-
-//Para los delanteros
-document.getElementById("inputNombrePortero").addEventListener("keyup", validarPortero);
-document.getElementById("inputCarnetPortero").addEventListener("keyup", validarPortero);
-document.getElementById("inputEdadPortero").addEventListener("keyup", validarPortero);
-document.getElementById("selectSexoPortero").addEventListener("change", validarPortero);
-
-/*Para el nombre del equipo
-document.getElementById("inputNombrePortero").addEventListener("keyup", validarPortero);
-document.getElementById("inputCarnetPortero").addEventListener("keyup", validarPortero);
-document.getElementById("inputEdadPortero").addEventListener("keyup", validarPortero);
-document.getElementById("selectSexoPortero").addEventListener("change", validarPortero);
-*/
-
-//Funciones de validación
-function validarEquipo(){
-    //Variables para la validar
-    let nombre;
-    vali = 0;
-
-    nombre = document.getElementById("inputNombreEquipo").value;
-
-    if (nombre == "") {
-        vali++;
-    }
-    if (vali == 0) {
-        document.getElementById("btnGuardarEquipo").disabled = false;
-    } else {
-        document.getElementById("btnGuardarEquipo").disabled = true;
-    }
+function addPortero(){
+    arrayNombres[0] = document.querySelector("#inputNombrePortero").value.toString();
+    arrayCarnet[0] = document.querySelector("#inputCarnetPortero").value.toString();
+    arrayEdad[0] = document.querySelector("#inputEdadPortero").value.toString();
+    arraySexo[0] = document.querySelector("#inputSexoPortero").value.toString();
 
 }
 
-function validarPortero(){
-    //variables para la validación
-    let nombre;
-    let carnet;
-    let edad;
-    let sexo;
-    vali = 0; //Nos servirá para validar
+function addDefensas(){
+    arrayNombres[1] = document.querySelector("#inputNombreDefensa1").value.toString();
+    arrayCarnet[1] = document.querySelector("#inputCarnetDefensa1").value.toString();
+    arrayEdad[1] = document.querySelector("#inputEdadDefensa1").value.toString();
+    arraySexo[1] = document.querySelector("#inputSexoDefensa1").value.toString();
 
-    //Asignacion de variables
-    nombre = document.getElementById("inputNombrePortero").value;
-    carnet = document.getElementById("inputCarnetPortero").value;
-    edad = document.getElementById("inputEdadPortero").value;
-    sexo = document.getElementById("selectSexoPortero").value;
+    arrayNombres[2] = document.querySelector("#inputNombreDefensa2").value.toString();
+    arrayCarnet[2] = document.querySelector("#inputCarnetDefensa2").value.toString();
+    arrayEdad[2] = document.querySelector("#inputEdadDefensa2").value.toString();
+    arraySexo[2] = document.querySelector("#inputSexoDefensa2").value.toString();
 
-    //Validaciones
-    if (nombre == "" || carnet == "" || edad == "" || sexo == "") {
-        vali++;
-    }
-    if (vali == 0) {
-        document.getElementById("btnGuardarPortero").disabled = false;
-    } else {
-        document.getElementById("btnGuardarPortero").disabled = true;
-    }
+    arrayNombres[3] = document.querySelector("#inputNombreDefensa3").value.toString();
+    arrayCarnet[3] = document.querySelector("#inputCarnetDefensa3").value.toString();
+    arrayEdad[3] = document.querySelector("#inputEdadDefensa3").value.toString();
+    arraySexo[3] = document.querySelector("#inputSexoDefensa3").value.toString();
+
+    arrayNombres[4] = document.querySelector("#inputNombreDefensa4").value.toString();
+    arrayCarnet[4] = document.querySelector("#inputCarnetDefensa4").value.toString();
+    arrayEdad[4] = document.querySelector("#inputEdadDefensa4").value.toString();
+    arraySexo[4] = document.querySelector("#inputSexoDefensa4").value.toString();
+
 }
 
-function validarDefensas(){
-    //variables para la validación
-    let nombre1, nombre2, nombre3, nombre4;
-    let carnet1, carnet2, carnet3, carnet4;
-    let edad1, edad2, edad3, edad4;
-    let sexo1, sexo2, sexo3, sexo4;
-    vali = 0; //Nos servirá para validar
+function addMedios(){
+    arrayNombres[5] = document.querySelector("#inputNombreMedio1").value.toString();
+    arrayCarnet[5] = document.querySelector("#inputCarnetMedio1").value.toString();
+    arrayEdad[5] = document.querySelector("#inputEdadMedio1").value.toString();
+    arraySexo[5] = document.querySelector("#inputSexoMedio1").value.toString();
 
-    //Asignacion de variables
-    nombre1 = document.getElementById("inputNombreDefensa1").value;
-    carnet1 = document.getElementById("inputCarnetDefensa1").value;
-    edad1 = document.getElementById("inputEdadDefensa1").value;
-    sexo1 = document.getElementById("selectSexoDefensa1").value;
+    arrayNombres[6] = document.querySelector("#inputNombreMedio2").value.toString();
+    arrayCarnet[6] = document.querySelector("#inputCarnetDMedio").value.toString();
+    arrayEdad[6] = document.querySelector("#inputEdadMedio2").value.toString();
+    arraySexo[6] = document.querySelector("#inputSexoMedio2").value.toString();
 
-    nombre2 = document.getElementById("inputNombreDefensa2").value;
-    carnet2 = document.getElementById("inputCarnetDefensa2").value;
-    edad2 = document.getElementById("inputEdadDefensa2").value;
-    sexo2 = document.getElementById("selectSexoDefensa2").value;
-    
-    nombre3 = document.getElementById("inputNombreDefensa3").value;
-    carnet3 = document.getElementById("inputCarnetDefensa3").value;
-    edad3 = document.getElementById("inputEdadDefensa3").value;
-    sexo3 = document.getElementById("selectSexoDefensa3").value;
+    arrayNombres[7] = document.querySelector("#inputNombreMedio3").value.toString();
+    arrayCarnet[7] = document.querySelector("#inputCarnetMedio3").value.toString();
+    arrayEdad[7] = document.querySelector("#inputEdadMedio3").value.toString();
+    arraySexo[7] = document.querySelector("#inputSexoMedio3").value.toString();
 
-    nombre4 = document.getElementById("inputNombreDefensa4").value;
-    carnet4 = document.getElementById("inputCarnetDefensa4").value;
-    edad4 = document.getElementById("inputEdadDefensa4").value;
-    sexo4 = document.getElementById("selectSexoDefensa4").value;
+    arrayNombres[8] = document.querySelector("#inputNombreMedio4").value.toString();
+    arrayCarnet[8] = document.querySelector("#inputCarnetMedio4").value.toString();
+    arrayEdad[8] = document.querySelector("#inputEdadMedio4").value.toString();
+    arraySexo[8] = document.querySelector("#inputSexoMedio4").value.toString();
 
-    //Validaciones
-    if (nombre1 == "" || carnet1 == "" || edad1 == "" || sexo1 == "" || nombre2 == "" || carnet2 == "" || edad2 == "" || sexo2 == "" || nombre3 == "" || carnet3 == "" || edad3 == "" || sexo3 == "" || nombre4 == "" || carnet4 == "" || edad4 == "" || sexo4 == "") {
-        vali++;
-    }
-    if (vali == 0) {
-        document.getElementById("btnGuardarDefensas").disabled = false;
-    } else {
-        document.getElementById("btnGuardarDefensas").disabled = true;
-    }
 }
 
-function validarMedios(){
-    //variables para la validación
-    let nombre1, nombre2, nombre3, nombre4;
-    let carnet1, carnet2, carnet3, carnet4;
-    let edad1, edad2, edad3, edad4;
-    let sexo1, sexo2, sexo3, sexo4;
-    vali = 0; //Nos servirá para validar
+function addDelante(){
+    arrayNombres[9] = document.querySelector("#inputNombreDelante1").value.toString();
+    arrayCarnet[9] = document.querySelector("#inputCarnetDelante1").value.toString();
+    arrayEdad[9] = document.querySelector("#inputEdadDelante1").value.toString();
+    arraySexo[9] = document.querySelector("#inputSexoDelante1").value.toString();
 
-    //Asignacion de variables
-    nombre1 = document.getElementById("inputNombreMedio1").value;
-    carnet1 = document.getElementById("inputCarnetMedio1").value;
-    edad1 = document.getElementById("inputEdadMedio1").value;
-    sexo1 = document.getElementById("selectSexoMedio1").value;
-
-    nombre2 = document.getElementById("inputNombreMedio2").value;
-    carnet2 = document.getElementById("inputCarnetMedio2").value;
-    edad2 = document.getElementById("inputEdadMedio2").value;
-    sexo2 = document.getElementById("selectSexoMedio2").value;
-    
-    nombre3 = document.getElementById("inputNombreMedio3").value;
-    carnet3 = document.getElementById("inputCarnetMedio3").value;
-    edad3 = document.getElementById("inputEdadMedio3").value;
-    sexo3 = document.getElementById("selectSexoDefensa3").value;
-
-    nombre4 = document.getElementById("inputNombreMedio4").value;
-    carnet4 = document.getElementById("inputCarnetMedio4").value;
-    edad4 = document.getElementById("inputEdadMedio4").value;
-    sexo4 = document.getElementById("selectSexoMedio4").value;
-
-    //Validaciones
-    if (nombre1 == "" || carnet1 == "" || edad1 == "" || sexo1 == "" || nombre2 == "" || carnet2 == "" || edad2 == "" || sexo2 == "" || nombre3 == "" || carnet3 == "" || edad3 == "" || sexo3 == "" || nombre4 == "" || carnet4 == "" || edad4 == "" || sexo4 == "") {
-        vali++;
-    }
-    if (vali == 0) {
-        document.getElementById("btnGuardarPortero").disabled = false;
-    } else {
-        document.getElementById("btnGuardarPortero").disabled = true;
-    }
+    arrayNombres[10] = document.querySelector("#inputNombreDelante2").value.toString();
+    arrayCarnet[10] = document.querySelector("#inputCarnetDelante2").value.toString();
+    arrayEdad[10] = document.querySelector("#inputEdadDelante2").value.toString();
+    arraySexo[10] = document.querySelector("#inputSexoDelante2").value.toString();
 }
-
-function validarDelanteros(){
-    //variables para la validación
-    let nombre1, nombre2, nombre3, nombre4;
-    let carnet1, carnet2, carnet3, carnet4;
-    let edad1, edad2, edad3, edad4;
-    let sexo1, sexo2, sexo3, sexo4;
-    vali = 0; //Nos servirá para validar
-
-    //Asignacion de variables
-    nombre1 = document.getElementById("inputNombreDelante1").value;
-    carnet1 = document.getElementById("inputCarnetDelante1").value;
-    edad1 = document.getElementById("inputEdadDelante1").value;
-    sexo1 = document.getElementById("selectSexoDelante1").value;
-
-    nombre2 = document.getElementById("inputNombreDelante2").value;
-    carnet2 = document.getElementById("inputCarnetDelante2").value;
-    edad2 = document.getElementById("inputEdadDelante2").value;
-    sexo2 = document.getElementById("selectSexoDelante2").value;
-    
-    nombre3 = document.getElementById("inputNombreDelante3").value;
-    carnet3 = document.getElementById("inputCarnetDelante3").value;
-    edad3 = document.getElementById("inputEdadDelante3").value;
-    sexo3 = document.getElementById("selectSexoDeDelante3").value;
-
-    nombre4 = document.getElementById("inputNombreDelante4").value;
-    carnet4 = document.getElementById("inputCarnetDelante4").value;
-    edad4 = document.getElementById("inputEdadDelante4").value;
-    sexo4 = document.getElementById("selectSexoDelante4").value;
-
-    //Validaciones
-    if (nombre1 == "" || carnet1 == "" || edad1 == "" || sexo1 == "" || nombre2 == "" || carnet2 == "" || edad2 == "" || sexo2 == "" || nombre3 == "" || carnet3 == "" || edad3 == "" || sexo3 == "" || nombre4 == "" || carnet4 == "" || edad4 == "" || sexo4 == "") {
-        vali++;
-    }
-    if (vali == 0) {
-        document.getElementById("btnGuardarDelante").disabled = false;
-    } else {
-        document.getElementById("btnGuardarDelante").disabled = true;
-    }
-}
-
-//Funciones que ejecutaran los botones
-
 
 
 })
